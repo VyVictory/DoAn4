30f62980d50fe6cf037d990e3119075b7b738d15
VyVictory <145247364+VyVictory@users.noreply.github.com>
2025-02-22 15:22:13 +0700

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,3 @@
 const express = require('express');
-const cors = require('cors');
 
-const app = express();
-
-// Enable CORS for all origins (or specific origins)
-app.use(cors({
-    origin: 'http://localhost:3000/', // Replace with your frontend URL if needed
-    methods: 'GET,POST,PUT,DELETE', // Specify allowed HTTP methods
-    allowedHeaders: 'Content-Type, Authorization', // Specify allowed headers
-}));
-app.use(cors());
-app.use(express.json());
-
-// Your routes go here
 require('./app');  // Your route file
